ef48ca4dda00c8ebb1029f696e785aa758b332e7
Oussama <oussamabouzalim942@gmail.com>
2023-04-27 17:17:16 +0200

    Updates

---
+++ precache-manifest.2e1d3cbc00c62dddd2ec29e1419d9612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4719d562f89be79208c",
+    "url": "/Lotka-Volterra/static/css/main.371c82de.chunk.css"
+  },
+  {
+    "revision": "f4719d562f89be79208c",
+    "url": "/Lotka-Volterra/static/js/main.f4719d56.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/Lotka-Volterra/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "2d5c1ca064c96d56f84a",
+    "url": "/Lotka-Volterra/static/js/2.2d5c1ca0.chunk.js"
+  },
+  {
+    "revision": "a6418df8a0806f5ec98e",
+    "url": "/Lotka-Volterra/static/js/runtime~main.a6418df8.js"
+  },
+  {
+    "revision": "d97731e61239493f929f9a775aa11a58",
+    "url": "/Lotka-Volterra/static/media/img.d97731e6.png"
+  },
+  {
+    "revision": "f8643db37031e741f307a442e18e67c2",
+    "url": "/Lotka-Volterra/index.html"
+  }
+];
